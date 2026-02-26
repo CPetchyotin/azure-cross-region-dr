0bfd902a1326dc4615083addee2595924aca469b
CPetchyotin <118425577+CPetchyotin@users.noreply.github.com>
2026-02-26 23:10:13 +0700

    cosmosdb connection string add

--- azure-sg-main/main.tf
+++ azure-sg-main/main.tf
@@ -162,3 +162,4 @@ resource "azurerm_cosmosdb_account" "db" {
 }
 
 
+
--- azure-sg-main/outputs.tf
+++ azure-sg-main/outputs.tf
@@ -1,3 +1,7 @@
 output "primary_public_ip" {
-value = azurerm_public_ip.pip.ip_address
+  value = azurerm_public_ip.pip.ip_address
+}
+output "cosmosdb_connection_string" {
+  value     = azurerm_cosmosdb_account.db.primary_sql_connection_string
+  sensitive = true
 }
